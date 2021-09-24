83572bab574d9c5f51140e14da9b4ed9c5affd78
gromomarketingupwork <gromomarketing.upwork@gmail.com>
2021-09-24 07:39:30 +0500

    feat: nft factory contract added

--- src/contract/NFT.sol
+++ src/contract/NFT.sol
@@ -1,11 +1,13 @@
 // SPDX-License-Identifier: MIT OR Apache-2.0
 pragma solidity ^0.8.7;
 
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract NFT is ERC721URIStorage {
+contract NFT is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
     address contractAddress;
@@ -14,13 +16,64 @@ contract NFT is ERC721URIStorage {
         contractAddress = marketplaceAddress;
     }
 
-    function mintToken(string memory tokenURI) public returns (uint) {
+    function mintToken(string memory _tokenURI) public returns (uint) {
         _tokenIds.increment();
         uint256 newItemId = _tokenIds.current();
 
         _mint(msg.sender, newItemId);
-        _setTokenURI(newItemId, tokenURI);
+        _setTokenURI(newItemId, _tokenURI);
         setApprovalForAll(contractAddress, true);
         return newItemId;
     }
+    
+    // The following functions are overrides required by Solidity.
+
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
+        internal
+        override(ERC721, ERC721Enumerable)
+    {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
+
+contract ContractFactoryNFT {
+
+    address[] public contracts;
+    address public lastContractAddress;       
+
+    function getContractCount() public view returns(uint contractCount) {
+        return contracts.length;
+    }
+
+    // deploy a new nft contract
+    function deploy(address marketplaceAddress, string memory name, string memory symbol) public returns(address newContract){
+         NFT nftContract = new NFT(marketplaceAddress, name, symbol);
+         address nftContractAddress = address(nftContract);
+         contracts.push(nftContractAddress);
+         lastContractAddress = nftContractAddress;
+
+         return nftContractAddress;
+    }        
 }
